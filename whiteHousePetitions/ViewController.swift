fbedbcb7594dbf80edef1096ac03fc6c37a0f5f9
josedlg <josedelaguarda@outlook.com>
2020-01-28 19:09:36 -0500

    Initial Commit

---
+++ whiteHousePetitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whiteHousePetitions
+//
+//  Created by Jose Delaguarda on 1/28/20.
+//  Copyright © 2020 DLGTECH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
